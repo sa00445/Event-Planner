eab846ea6189bf2f22a8545ff9dae3e2c6104aff
Syed Abdullah Ali <mtuais95@gmail.com>
2020-08-24 06:33:02 +0400

    add contact route and fixed other, copied navbar across all pages

--- server.js
+++ server.js
@@ -4,15 +4,18 @@ const path = require('path');
 
 const homePage = fs.readFileSync('index.html');
 const aboutPage = fs.readFileSync('about.html');
+const contactPage = fs.readFileSync('contact.html');
 const notFoundPage = fs.readFileSync('notfound.html');
 // const homePage = fs.readFileSync('index.html');
 // const homePage = fs.readFileSync('index.html');
 
 const server = http.createServer((req, res) => {
-    if (req.url === "/") {
+    if (req.url === "/" || req.url === "/index.html") {
         res.end(homePage);
     } else if (req.url === "/about.html") {
         res.end(aboutPage);
+    } else if (req.url === "/contact.html") {
+        res.end(contactPage);
     } else {
         res.writeHead(404);
         res.end(notFoundPage);
