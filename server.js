8bdb9e23ba85bee93d22e1ace0a142f4bd481916
Syed Abdullah Ali <mtuais95@gmail.com>
2020-08-24 06:21:51 +0400

    create navbar using flexbox, I have placed my styling within the html files <style> tags, since I have not been able to figure out a way to serve static files using vanilla node

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ const notFoundPage = fs.readFileSync('notfound.html');
 // const homePage = fs.readFileSync('index.html');
 // const homePage = fs.readFileSync('index.html');
 
-const server = http.createServer((req,res) => {
+const server = http.createServer((req, res) => {
     if (req.url === "/") {
         res.end(homePage);
-    } else if (req.url === "/about") {
+    } else if (req.url === "/about.html") {
         res.end(aboutPage);
     } else {
         res.writeHead(404);
